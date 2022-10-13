02a08daa26f1f69ac42c9cf9e42796e88aaed0b0
olivia pasion <oap9820@gmail.com>
2022-10-13 15:26:48 -0700

    worked on practice problems to better understand Python syntax

---
+++ practice-problems/average.py
@@ -0,0 +1,12 @@
+print("Please enter five integers")
+
+num_1 = int(input())
+num_2 = int(input())
+num_3 = int(input())
+num_4 = int(input())
+num_5 = int(input())
+
+sumInt = num_1 + num_2 + num_3 + num_4 + num_5
+avgInt = sumInt/5
+
+print(avgInt);
---
+++ practice-problems/temp_convert.py
@@ -0,0 +1,11 @@
+# Write a program that converts Celsius temps to fahrenheit
+# Formula F = (9/5)C + 32
+
+C = int(input(
+  "Provide a temperature in celsius to be Converted to Fahrenheit "
+))
+
+F = ((9/5)*int(C) + 32);
+
+print(F)
+  
